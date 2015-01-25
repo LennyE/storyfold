8b8ad7ac786f35321b8541bc40ae5f0cc199bbb1
Lenny Ekberg <Lenny@lennys-mbp.lan>
2015-01-25 18:15:41 +0100

    Basic foundation
    Includes removal of formatting.

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="utf-8">
+	<title>Storyfold! &alpha;</title>
+
+	<!-- stilmallar --> 
+	<link href="css/style.css" rel="stylesheet" type="text/css" id="stl" />
+	<script src="js/jquery-1.8.2.min.js"></script>
+	
+	<!-- google fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,400,300,600' rel='stylesheet' type='text/css'>
+	
+</head>
+<body>
+	
+<div id="results">
+
+	
+	<div contenteditable="true" class="no-formatting">&#133;</div>
+
+	<!-- 	<?php include('data.php'); ?> -->
+
+</div>
+
+	<!-- remove formatting -->
+	<script id="jsbin-javascript" src="js/remove-paste-formatting.js"></script>
+	
+</body>
+</html>
+
+
---
+++ js/remove-paste-formatting.js
@@ -0,0 +1,5 @@
+$('.no-formatting[contenteditable]').on('paste',function(e) {
+    e.preventDefault();
+    var text = (e.originalEvent || e).clipboardData.getData('text/plain') || prompt('Paste something..');
+    window.document.execCommand('insertText', false, text);
+});
